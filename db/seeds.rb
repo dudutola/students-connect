03f7e81793c82eab4bc44da560ba9b3fac70b88b
Dulcelene Machado <dudutola@gmail.com>
2025-04-25 16:12:07 +0100

    Added odin_curriculum.json file with the odin data Updated seeds to parse JSON and create odin chapter records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+
+puts "Cleaning everything..."
+
+# Delete dependent records first
+Chapter.destroy_all
+
+# Now, delete the users
+User.destroy_all
+
+puts "Database cleaned."
+
+
+odin_filepath = File.join(__dir__, "odin_curriculum.json")
+serialized_curriculum = File.read(odin_filepath)
+odin_chapters = JSON.parse(serialized_curriculum)
+
+odin_chapters.each do |chapter|
+  new_chapter = Chapter.find_or_create_by!(
+    name: chapter["chapter_title"],
+    icon: chapter["chapter_icon"],
+    description: chapter["chapter_description"],
+    overview: chapter["chapter_overview"],
+    source: "the_odin_project"
+  )
+  puts "Created chapter #{new_chapter.name}"
+end
+
+puts "Everything created for The Odin Project!"

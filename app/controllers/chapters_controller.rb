3421b57722ed5a4b2edfe6b32396d9af23229327
Dulcelene Machado <dudutola@gmail.com>
2025-04-26 12:17:22 +0100

    Added chapters#show with previous/next navigation logic Added links to chapters#show pages from chapters#index Updated chapter_policy to authorize show action

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -8,4 +8,15 @@ class ChaptersController < ApplicationController
       @chapters = policy_scope(Chapter.where(source: "le_wagon"))
     end
   end
+
+  def show
+    @chapter = Chapter.find(params[:id])
+    authorize @chapter
+
+    all_chapters = Chapter.where(source: @chapter.source).order(:id)
+    current_chapter_index = all_chapters.find_index(@chapter)
+
+    @previous_chapter = all_chapters[current_chapter_index - 1] if current_chapter_index > 0
+    @next_chapter = all_chapters[current_chapter_index + 1] if current_chapter_index < all_chapters.length - 1
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :chapters, only: [ :index ]
+  resources :chapters, only: [ :index, :show ]
 end
